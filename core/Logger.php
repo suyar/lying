b1e16bf02efb76cc3d41c1ea5e5ae1de8776d5dd
CarolKey <296399959@qq.com>
2016-09-05 13:26:00 +0800

    修改log类为收集机制,等到log条数到了一定数量才写出文件,减少文件IO的使用

--- core/Logger.php
+++ core/Logger.php
@@ -4,17 +4,47 @@ class Logger {
     
     private static $logger;
     
+    /**
+     * 单个日志文件大小限制,默认1M
+     * @var int
+     */
     private $maxSize;
     
-    private function __construct() {
-        $this->maxSize = isset(\App::$config['log']['maxSize']) ? \App::$config['log']['maxSize'] : 1*1024*1024;
+    /**
+     * 收集多少条log后输出到文件;
+     * log并不是直接输出到文件,而是先存在数组里面,等到条数超过$maxLenth后才会写入到文件,并且重新收集;
+     * 这个值不建议设置太大,否则会占用太多内存,如果日志的数据太大,请把条数设置少一点,默认为20条
+     * @var int
+     */
+    private $maxLenth;
+    
+    /**
+     * 用来存log信息的数组
+     * @var array
+     */
+    private $logArr = [];
+    
+    private function __construct($config) {
+        $this->maxSize = isset($config['maxSize']) ? $config['maxSize'] : 1*1024*1024;
+        $this->maxLenth = isset($config['maxLenth']) ? $config['maxLenth'] : 20;
+    }
+    
+    /**
+     * 变量被销毁的时候输出$logArr里的所有信息
+     */
+    public function __destruct() {
+        foreach ($this->logArr as $fileName=>$data) {
+            $file = $this->getLogFile($fileName);
+            file_put_contents($file, $data, FILE_APPEND|LOCK_EX);
+            unset($this->logArr[$fileName]);
+        }
     }
     
     private function __clone() {}
     
     public static function getInstance() {
         if (!self::$logger instanceof self) {
-            self::$logger = new self;
+            self::$logger = new self(isset(\App::$config['log']) ? \App::$config['log'] : []);
         }
         return self::$logger;
     }
@@ -23,10 +53,9 @@ class Logger {
      * 写日志
      * @param mixed $var 要输出的变量
      * @param string $fileName 日志文件名
-     * @return number
+     * @param boolean $flush 是否立即输出所有log(会降低性能;如果log是大数据的话,可以选用此项来减少内存的使用)
      */
-    public function log($var, $fileName = 'default') {
-        $fileName = $this->getLogFile($fileName);
+    public function log($var, $fileName = 'default', $flush = false) {
         if (!is_string($var)) {
             ob_start();
             var_export($var);
@@ -37,7 +66,10 @@ class Logger {
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
         $target = "in {$trace[0]['file']} on line {$trace[0]['line']}\r\n";
         $content = "[$time][$ip]$target{$var}\r\n";
-        return file_put_contents($fileName, $content, FILE_APPEND|LOCK_EX);
+        $this->logArr[$fileName][] = $content;
+        if (count($this->logArr[$fileName], COUNT_RECURSIVE) >= $this->maxLenth || $flush) {
+            $this->__destruct();
+        }
     }
     
     /**
