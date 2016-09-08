7edabf7a0a58f46cace01d82dea3586db427bbc3
CarolKey <296399959@qq.com>
2016-09-08 20:56:18 +0800

    修改路由获取GET参数BUG

--- core/Router.php
+++ core/Router.php
@@ -12,8 +12,11 @@ class Router {
      * @return array
      */
     public static function parse() {
-        $path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : (isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : '/');
-        if ($path !== '/' && !preg_match('/[a-zA-Z0-9]+\.html$/', basename($path))) throw new \Exception("Page not found", 404);
+        $request_uri = $_SERVER['REQUEST_URI'];
+        $uri_info = parse_url($request_uri);
+        $path = $uri_info['path'];
+        $query = isset($uri_info['query']) ? $uri_info['query'] : false;
+        if ($path !== '/' && !preg_match('/[a-zA-Z0-9]+\.html$/', basename($path))) throw new \Exception('Page not found', 404);
         $path = trim(str_replace('.html', '', $path), '/');
         $path = $path === '' ? [] : explode('/', $path);
         switch (count($path)) {
@@ -38,21 +41,21 @@ class Router {
         define('__MODULE__', $m);
         define('__CONTROLLER__', strtolower((new \ReflectionClass($class))->getShortName()));
         define('__ACTION__', $a);
-        self::collectReq($path);
+        self::collectGet($path, $query);
         return [$class, $a];
     }
     
     /**
      * 把PATHINFO里的参数解析出来
      * @param array $path
+     * @param string $query
      */
-    public static function collectReq($path) {
-        isset($_GET) && $_GET ? array_shift($_GET) : '';
+    public static function collectGet($path, $query) {
+        $_GET = [];
+        if ($query) parse_str($query, $_GET);
         for ($i = 0; isset($path[$i]); $i += 2) {
             if($path[$i] === '')  continue;
             $_GET[$path[$i]] = isset($path[$i + 1]) ? $path[$i + 1] : '';
         }
     }
-    
-    
 }
