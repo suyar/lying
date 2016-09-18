9cd10f0cb0cbac81bda0d77a86c1047de0cbe9ba
CarolKey <296399959@qq.com>
2016-09-18 13:14:46 +0800

    webhooks注释更正

--- extend/Webhooks.php
+++ extend/Webhooks.php
@@ -9,7 +9,7 @@ class Webhooks {
     /**
      * 验证github的webhooks推送
      * @param string $secret hmac的密钥
-     * @param boolean $reJsonStr 是否返回推送的数据(默认返回boolean)
+     * @param boolean $returnData 是否返回推送的数据(默认返回boolean)
      * @return string|boolean 成功返回ture或者数据,失败返回false
      */
     public static function validate($secret, $returnData = false) {
