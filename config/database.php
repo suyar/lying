6a25c7094a04cda9676ca9aee5c7ad8af4d59351
CarolKey <296399959@qq.com>
2016-11-07 23:45:55 +0800

    rewrite

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db' => [
-        'dsn'  => 'mysql:host=127.0.0.1;dbname=e',
-        'username' => 'root',
-        'password'  => 'root',
-        'charset' => 'utf8'
-    ],
-];
