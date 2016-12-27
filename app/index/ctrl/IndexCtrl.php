27cd6b7cd11a06fd32c2af07e0f04e4ed49596fe
CarolKey <296399959@qq.com>
2016-12-27 22:39:45 +0800

    tmp

--- app/index/ctrl/IndexCtrl.php
+++ app/index/ctrl/IndexCtrl.php
@@ -14,7 +14,16 @@ class IndexCtrl extends Ctrl
     
     public function index()
     {
-        maker()->logger()->log(['尤里', '苏', 'name'=>['666', 'sex'=>false]]);
+        //var_dump($_SERVER);
+        //maker()->cookie()->set('name', 'suyaqi');
+        $key = '123';
+        $data = 'suyaqi';
+        $key = $iv = md5($key, true);
+        $res = openssl_encrypt($data, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+        $res = base64_encode($res);
+        var_dump($res);
+        $res = openssl_decrypt(base64_decode($res), 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+        var_dump($res);
     }
     
     
