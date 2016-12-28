c443560ab301e79d60aaffd41ecf1d99d8c03bcd
CarolKey <296399959@qq.com>
2016-12-28 19:35:06 +0800

    tmp

--- app/index/ctrl/IndexCtrl.php
+++ app/index/ctrl/IndexCtrl.php
@@ -14,10 +14,26 @@ class IndexCtrl extends Ctrl
     
     public function index()
     {
-        
-        
+        $str = 'sdfdsf你的 是的呢是你的 是的呢是你';
+        $key = '123456';
+        $this->encrype($str, $key);
     }
     
+    public function encrype($str, $key)
+    {
+        $key = md5($key);
+        $publicKey = microtime(true);
+        //var_dump($publicKey);
+        
+        $keystore = bin2hex($str);
+        var_dump($keystore);
+        
+        $strLen = strlen($str);
+        for ($i = 0; $i < $strLen; $i++) {
+            
+            //echo ord($str[$i]) . "<br>";
+        }
+    }
     
     
 }
