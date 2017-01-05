797a2ee5807b48006ee6235304ddeaeaca42d5e7
CarolKey <296399959@qq.com>
2017-01-05 21:54:38 +0800

    tmp

--- vendor/lying/classes.php
+++ vendor/lying/classes.php
@@ -12,6 +12,7 @@ return [
     'lying\cache\MemCached' => DIR_LYING . '/cache/MemCached.php',
     
     'lying\db\Connection' => DIR_LYING . '/db/Connection.php',
+    'lying\db\Query' => DIR_LYING . '/db/Query.php',
     'lying\db\QueryBuilder' => DIR_LYING . '/db/QueryBuilder.php',
     'lying\db\Schema' => DIR_LYING . '/db/Schema.php',
     
---
+++ vendor/lying/db/Query.php
@@ -0,0 +1,31 @@
+<?php
+namespace lying\db;
+
+class Query
+{
+    
+    protected $select;
+    
+    protected $from;
+    
+    protected $distinct;
+    
+    protected $join;
+    
+    protected $where;
+
+    protected $groupBy;
+    
+    protected $having;
+    
+    protected $orderBy;
+    
+    protected $limit;
+    
+    
+    public function select()
+    {
+        
+    }
+    
+}
