b28c9f0b2ab38b1ac0424de968c1c99d67f7bb38
CarolKey <296399959@qq.com>
2017-05-01 15:13:00 +0800

    应用相对路径相对于入口文件所在目录

--- kernel/init.php
+++ kernel/init.php
@@ -1,17 +1,17 @@
 <?php
 header('Content-Type:text/html; charset=UTF-8');
 
-define('DIR_ROOT', realpath(__DIR__ . '/../'));
-
 define('DIR_KERNEL', __DIR__);
+
+define('DIR_WEB', dirname($_SERVER['SCRIPT_FILENAME']));
+
+define('DIR_ROOT', dirname(DIR_WEB));
 
 define('DIR_CONFIG', DIR_ROOT . DIRECTORY_SEPARATOR . 'config');
 
 define('DIR_MODULE', DIR_ROOT . DIRECTORY_SEPARATOR . 'module');
 
 define('DIR_RUNTIME', DIR_ROOT . DIRECTORY_SEPARATOR . 'runtime');
-
-define('DIR_WEB', dirname($_SERVER['SCRIPT_FILENAME']));
 
 require __DIR__ . '/Lying.php';
 
