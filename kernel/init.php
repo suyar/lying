07b68d303a47a42220659dba55d6843f4a0843dc
carolkey <git@suyaqi.cn>
2017-06-18 07:52:47 +0800

    将cli入口移出web目录，方式用户执行

--- kernel/init.php
+++ kernel/init.php
@@ -7,7 +7,7 @@ define('DIR_KERNEL', __DIR__);
 
 define('DIR_WEB', dirname($_SERVER['SCRIPT_FILENAME']));
 
-define('DIR_ROOT', dirname(DIR_WEB));
+define('DIR_ROOT', php_sapi_name() === 'cli' ? DIR_WEB : dirname(DIR_WEB));
 
 define('DIR_CONFIG', DIR_ROOT . DS . 'config');
 
