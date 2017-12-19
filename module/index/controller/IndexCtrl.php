053248c385d070f49a25f96af97c281d792ad7d1
suyar <su@zorzz.com>
2017-12-19 23:36:23 +0800

    重构路由

--- module/index/controller/IndexCtrl.php
+++ module/index/controller/IndexCtrl.php
@@ -10,20 +10,23 @@ class IndexCtrl extends Controller
     public function index()
     {
         //$path以'/'开头或者为空,这种不会反解析路由规则
-        echo \Lying::$maker->router()->createUrl1('', ['a'=>1, 'b'=>2]);
+        echo \Lying::$maker->router()->createUrl('', ['a'=>1, 'b'=>2]);
         echo "<br>\n";
 
-        echo \Lying::$maker->router()->createUrl1('/', ['a'=>1, 'b'=>2]);
+        echo \Lying::$maker->router()->createUrl('/', ['a'=>1, 'b'=>2]);
         echo "<br>\n";
 
-        echo \Lying::$maker->router()->createUrl1('/user', ['a'=>1, 'b'=>2]);
+        echo \Lying::$maker->router()->createUrl('/user', ['a'=>1, 'b'=>2]);
         echo "<br>\n";
 
-        echo \Lying::$maker->router()->createUrl1('/user/info.html', ['a'=>1, 'b'=>2]);
+        echo \Lying::$maker->router()->createUrl('/user/info.html', ['a'=>1, 'b'=>2]);
         echo "<br>\n";
 
         //$path不以'/'开头
-        echo \Lying::$maker->router()->createUrl1('admin/blog/get', ['id'=>1, 'name'=>'susu', 'num'=>[1,2]]);
+        echo \Lying::$maker->router()->createUrl('admin/blog/get', ['id'=>1, 'name'=>'susu', 'num'=>23]);
+        echo "<br>\n";
+
+        echo \Lying::$maker->router()->createUrl('login/index', ['id'=>1, 'name'=>'susu', 'num'=>23], true, true);
         echo "<br>\n";
 
 
