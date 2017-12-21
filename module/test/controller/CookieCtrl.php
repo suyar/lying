db4842de4f92d9458bcf8bbcf89629a2bf104983
suyar <su@zorzz.com>
2017-12-21 14:06:53 +0800

    调试cookie

---
+++ module/test/controller/CookieCtrl.php
@@ -0,0 +1,45 @@
+<?php
+namespace module\test\controller;
+
+use lying\service\Controller;
+use lying\service\Cookie;
+
+/**
+ * Class CookieCtrl
+ * @package module\test\controller
+ */
+class CookieCtrl extends Controller
+{
+    /**
+     * @var Cookie
+     */
+    private $cookie;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->cookie = \Lying::$maker->cookie();
+    }
+
+    public function set()
+    {
+        $result = $this->cookie->setExpire(time() + 10)->set('username', ['id'=>1, 'name'=>'苏亚琦']);
+        var_dump($result);
+    }
+
+    public function get()
+    {
+        $result = $this->cookie->get('username');
+        var_dump($result);
+    }
+
+
+    public function remove()
+    {
+        $result = $this->cookie->remove('username');
+        var_dump($result);
+    }
+}
