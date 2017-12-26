4f77e62c2ce8425ccde7093555a9583c040e6c38
su <su@revoke.cc>
2017-12-26 18:24:17 +0800

    完成类级别事件

--- kernel/service/Event.php
+++ kernel/service/Event.php
@@ -29,18 +29,69 @@ class Event
      */
     public $_data;
 
-    final public static function hook()
-    {
+    /**
+     * @var array 类事件容器
+     */
+    private static $_events = [];
 
+    /**
+     * 绑定一个函数到某个事件
+     * @param string $class 绑定的完整类名
+     * @param string $id 事件ID
+     * @param callable $callback 事件触发函数
+     * @param mixed $data 传递到事件触发器的数据
+     */
+    final public static function hook($class, $id, callable $callback, $data = null)
+    {
+        self::$_events[$id][$class][] = [$callback, $data];
     }
 
-    final public static function trigger()
+    /**
+     * 触发一个事件,如果事件返回false的话就不再继续执行后面绑定的事件了
+     * @param string|object $class 绑定的完整类名
+     * @param string $id 事件ID
+     * @param Event|null $event 事件实例
+     */
+    final public static function trigger($class, $id, Event $event = null)
     {
-
+        $event === null && ($event = new Event());
+        if (is_object($class)) {
+            $event->_sender = $class;
+            $class = get_class($class);
+        }
+        if (isset(self::$_events[$id][$class])) {
+            foreach (self::$_events[$id][$class] as $call) {
+                $event->_data = $call[1];
+                if (false === call_user_func($call[0], $event)) {
+                    break;
+                }
+            }
+        }
     }
 
-    final public static function unhook()
+    /**
+     * 移除事件
+     * @param string $class 绑定的完整类名
+     * @param string $id 事件ID
+     * @param callable|null $callback 事件触发函数
+     * @return bool 成功返回true,失败返回false
+     */
+    final public static function unhook($class, $id, callable $callback = null)
     {
-
+        if (isset(self::$_events[$id][$class])) {
+            if ($callback === null) {
+                unset(self::$_events[$id][$class]);
+                return true;
+            } else {
+                foreach (self::$_events[$id][$class] as $key => $event) {
+                    if ($event[0] === $callback) {
+                        unset(self::$_events[$id][$class][$key]);
+                        self::$_events[$id][$class] = array_values(self::$_events[$id][$class]);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
