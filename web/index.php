7fe89c18f9b245d44f8942fd6574e0862fdef961
suyar <su@zorzz.com>
2018-04-22 23:18:40 +0800

    rebuild

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
 version_compare(PHP_VERSION, '5.5.0', '>=') || die('Lying requires 5.5.0 or higher PHP version :)');
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../kernel/init.php';
 Lying::run(require DIR_CONFIG . '/web.php');
