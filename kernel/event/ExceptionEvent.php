592a4752c6cc72be3f43c6b9f1576a4a7aec7e04
suyar <su@zorzz.com>
2018-05-07 19:49:12 +0800

    rebuild

---
+++ kernel/event/ExceptionEvent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author carolkey <su@revoke.cc>
+ * @link https://github.com/carolkey/lying
+ * @copyright 2018 Lying
+ * @license MIT
+ */
+
+namespace lying\event;
+
+use lying\service\Event;
+
+/**
+ * Class ExceptionEvent
+ * @package lying\event
+ */
+class ExceptionEvent extends Event
+{
+    /**
+     * @var \Exception|\Throwable
+     */
+    public $e;
+
+    /**
+     * @var bool
+     */
+    public $stop = true;
+}
