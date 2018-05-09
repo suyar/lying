4ae7cf3ba971f71c139bff24e507eeaebcd34a85
suyar <su@zorzz.com>
2018-05-10 00:04:11 +0800

    rebuild

---
+++ web/test.php
@@ -0,0 +1,15 @@
+<?php
+header('HTTP/1.1 200 OK');
+var_dump(headers_list());
+//ob_end_flush();
+
+//headers_sent($file, $line);
+//var_dump($file,$line);
+
+
+
+//header('X-Bar: bar');
+//header('X-Far: far');
+
+
+
