3c05e097bbde6b259a144a89f8e3a5eaf516f55e
su <su@revoke.cc>
2018-05-09 19:51:59 +0800

    rebuild

--- kernel/service/Response.php
+++ kernel/service/Response.php
@@ -14,5 +14,55 @@ namespace lying\service;
  */
 class Response extends Service
 {
+    /**
+     * @var string HTTP版本
+     */
+    private $_version;
+
+    /**
+     * @var array 要发送的头
+     */
+    private $_headers = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (isset($_SERVER['SERVER_PROTOCOL']) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.0') {
+            $this->_version = '1.0';
+        } else {
+            $this->_version = '1.1';
+        }
+    }
+
+    /**
+     * 设置要发送的头
+     * @param string $name header名
+     * @param string $value header值
+     * @return $this
+     */
+    public function setHeader($name, $value)
+    {
+        $this->_headers[strtolower($name)] = $value;
+        return $this;
+    }
+
+    /**
+     * 发送已经设置的头信息,如果头已经发送了,那么这些头不会再发送
+     * @return $this
+     */
+    private function sendHeaders()
+    {
+        if (!headers_sent($file,$line)) {
+            foreach ($this->_headers as $name => $value) {
+                $name = str_replace(' ', '-', ucwords(str_replace('-', ' ', $name)));
+                header("$name: $value");
+            }
+        }
+        return $this;
+    }
+
 
 }
