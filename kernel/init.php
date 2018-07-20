a7943ed3cf91ad33bf41a7533db4e8355743088d
su <su@revoke.cc>
2018-07-20 16:01:43 +0800

    DIR_WEB修改为DOCUMENT_ROOT

--- kernel/init.php
+++ kernel/init.php
@@ -5,7 +5,7 @@ define('DIR_KERNEL', __DIR__);
 
 define('DIR_ROOT', dirname(DIR_KERNEL));
 
-define('DIR_WEB', DIR_ROOT . DS . 'web');
+define('DIR_WEB', realpath($_SERVER['DOCUMENT_ROOT']));
 
 define('DIR_CONFIG', DIR_ROOT . DS . 'config');
 
