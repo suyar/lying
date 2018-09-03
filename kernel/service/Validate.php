f729169d649a9e94ecfcef70c8e4d8bb72706961
su <su@revoke.cc>
2018-09-03 18:21:34 +0800

    开发验证器

--- kernel/service/Validate.php
+++ kernel/service/Validate.php
@@ -14,21 +14,49 @@ namespace lying\service;
  */
 class Validate extends Service
 {
+    /**
+     * @var array 校验规则
+     */
+    private $_rules = [];
+
+    /**
+     * @inheritdoc
+     */
+    final protected function init()
+    {
+        parent::init();
+        self::rules($this);
+    }
+
     protected static function rules(Validate $validate)
     {
         $validate->addRule('name', '', '', '')
-            ->addRule();
+            ->addRule('password', '', '', '');
     }
 
 
+    /**
+     * 添加校验规则
+     * @param string $column 字段名
+     * @param string|array $rule 规则
+     * @param string $msg 错误提示
+     * @param string|array $scene 字段校验场景
+     * @return $this
+     */
     public function addRule($column, $rule, $msg, $scene = null)
     {
-
+        $this->_rules[] = [$column, (array)$rule, $msg, (array)$scene];
         return $this;
     }
 
-    public function check($scene = null)
-    {
 
+    public function check($data, $scene = null)
+    {
+        foreach ($this->_rules as $ruleArr) {
+            list($column, $rule, $msg, $sceneArr) = $ruleArr;
+            if (in_array($scene, $sceneArr)) {
+
+            }
+        }
     }
 }
