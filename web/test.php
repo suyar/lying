0938276ecbede3ab4416440627768e47cc44020c
su <su@revoke.cc>
2021-07-17 13:53:40 +0800

    rebuild db

--- web/test.php
+++ web/test.php
@@ -1,3 +1,17 @@
 <?php
 
 
+
+
+
+
+$dsn = 'mysql:host=121.196.204.163;dbname=lying;charset=utf8';
+$user = 'root';
+$pass = 'Suyaqi1992#';
+
+$pdo = new \PDO($dsn, $user, $pass);
+
+$statement = $pdo->prepare('insert into user (id,username) values (10,"testname1")');
+$statement->execute();
+
+echo $pdo->lastInsertId('username');
