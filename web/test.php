4ae904b604e975da7470187c43d473d187025494
suyar <su@zorzz.com>
2021-08-16 12:19:45 +0800

    更改header操作

--- web/test.php
+++ web/test.php
@@ -1,5 +1,11 @@
 <?php
 
-$options = 'Any-Latin; Latin-ASCII; NFD; [:Nonspacing Mark:] Remove; NFC;';
+function t()
+{
+    var_dump(http_get_request_headers());
+    return [1,2,3,4,5,6,7,8,9,0];
+}
 
-echo transliterator_transliterate($options, "你好啊");
+foreach (t() as $t) {
+    var_dump($t);
+}
